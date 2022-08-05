58837836f272277e15df9f7b5219986efb861a0e
unknown <alan.diaz@bglobalsolutions.com>
2022-08-04 23:20:50 -0300

    Desafio Restful

--- product.js
+++ product.js
@@ -5,19 +5,22 @@ class Product {
         this.path = path
     }
 
-    async save(obj){
-        let file = await fs.promises.readFile(this.path, 'utf8');
-        let fileParse = JSON.parse(file);
-        if(fileParse.length){
-            let lastProdId = fileParse[fileParse.length - 1].id
-            await fs.promises.writeFile(this.path, JSON.stringify([...fileParse, {...obj, id: lastProdId + 1}]), null, 2);
-        }
-        else {
-            await fs.promises.writeFile(this.path, JSON.stringify([{...obj, id: 1}]), null, 2);
-        }
+    async save(obj) {
+        try {
+            let file = await fs.promises.readFile(this.path, 'utf8');
+            let fileParse = JSON.parse(file);
+            if (fileParse.length) {
+                let lastProdId = fileParse[fileParse.length - 1].id
+                await fs.promises.writeFile(this.path, JSON.stringify([...fileParse, { ...obj, id: lastProdId + 1 }]), null, 2);
+            }
+            else {
+                await fs.promises.writeFile(this.path, JSON.stringify([{ ...obj, id: 1 }]), null, 2);
+            }
 
-        return fileParse.length + 1;
-        console.log(file)
+            return fileParse.length + 1;
+        } catch (error) {
+            throw error
+        }
     }
 
     async getById(productId){
@@ -54,13 +57,13 @@ class Product {
             if(prod){
                 let fileParseFiltered = fileParse.filter(p => p.id !== id);
                 await fs.promises.writeFile(this.path, JSON.stringify(fileParseFiltered, null, 2))
-                console.log("Producto eliminado")
+                return ({message: "Producto eliminado"})
             }
             else {
-                console.log("No se encontro el producto");
+                throw "No se encontro el producto";
             }
         } catch (error) {
-            
+            throw error
         }
     }
 
@@ -69,6 +72,28 @@ class Product {
             await fs.promises.writeFile(this.path, JSON.stringify([], null ,2), 'utf8')
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    async update(obj){
+        try {
+            console.log(this.path)
+            let file = await fs.promises.readFile(this.path, "utf-8")
+            console.log(file)
+            let fileParse = JSON.parse(file)
+            console.log(obj.id)
+            console.log(fileParse)
+            const objIndex = fileParse.findIndex(prod => prod.id === obj.id)
+            console.log(objIndex)
+            if(objIndex !== -1){
+                fileParse[objIndex] = obj
+                await fs.promises.writeFile(this.path,JSON.stringify(fileParse,null,2))
+                return {message:"producto actualizado"}
+            }else{
+                throw "No existe el producto"
+            }
+        } catch (error) {
+            throw error
         }
     }
 }
--- server.js
+++ server.js
@@ -1,41 +1,136 @@
 const express = require("express");
+const app = express();
+const { Router} = express
+const router = Router()
 const Product = require("./product");
 const prod = new Product("./products.txt");
-const app = express();
 const PORT = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Bienvenido al servidor Express')
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static("public"))
+app.use("/api/products", router)
+
+//#region
+// app.get('/', (req, res) => {
+//     res.send('Bienvenido al servidor Express')
+// })
+
+// let countVisitas = 0;
+// app.get('/visitas', (req, res) => {
+//     countVisitas++;
+//     res.send(`La cantidad de visitas es: ${countVisitas}`)
+// })
+
+// app.get('/fyh', (req, res) => {
+//     let date = new Date;
+//     let dateFormat = [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('/') + ' ' +
+//                      [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
+
+//     res.send(`La fecha y hora actual es: ${dateFormat}`);
+// })
+
+// app.get('/productos', async (req, res) => {
+//     const listProducts = await prod.getAll();
+//     res.send(listProducts)
+// })
+
+// app.get('/productoRandom', async (req, res) => {
+//     const listProducts = await prod.getAll();
+//     const idProdAleatorio = Math.floor(Math.random() * listProducts.length);
+//     const prodRandom = listProducts[idProdAleatorio];
+//     res.send(prodRandom)
+// })
+//#endregion
+
+router.get("/", async (req,res)=>{
+    try {
+        const listProds = await prod.getAll()
+        res.json({
+            result: listProds   
+        })
+    } catch (error) {
+        res.json({
+            error
+        })
+    }
+})
+router.get("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const prodById = await prod.getById(Number(id))
+        res.json({
+            result: prodById
+        })
+
+    } catch (error) {
+        res.json({
+            error
+        })
+    }
 })
 
-let countVisitas = 0;
-app.get('/visitas', (req, res) => {
-    countVisitas++;
-    res.send(`La cantidad de visitas es: ${countVisitas}`)
+router.post("/", async(req,res)=>{
+    try {
+        const {title,price,thumbnail} = req.body
+        const idProdSaved = await prod.save({title,price,thumbnail})
+        res.json({ result: {
+            title,
+            price,
+            thumbnail,
+            idProdSaved
+        }
+        })
+        
+    } catch (error) {
+        res.json({
+            error
+        })
+    }
 })
 
-app.get('/fyh', (req, res) => {
-    let date = new Date;
-    let dateFormat = [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('/') + ' ' +
-                     [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
-
-    res.send(`La fecha y hora actual es: ${dateFormat}`);
+router.put("/:id", async(req,res)=>{
+    try {
+        const objProd = req.body
+        const {id} = req.params
+        const result = await prod.update({id:id,...objProd})
+        res.json({
+            result: result
+        })
+        
+    } catch (error) {
+        res.json({
+            error
+        })
+    }
 })
 
-app.get('/productos', async (req, res) => {
-    const listProducts = await prod.getAll();
-    res.send(listProducts)
+router.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await prod.delete(Number(id))
+        res.json({
+            result: result
+        })
+    } catch (error) {
+        res.json({
+            error
+        })
+    }
 })
 
-app.get('/productoRandom', async (req, res) => {
-    const listProducts = await prod.getAll();
-    const idProdAleatorio = Math.floor(Math.random() * listProducts.length);
-    const prodRandom = listProducts[idProdAleatorio];
-    res.send(prodRandom)
+app.listen(PORT, err =>{
+    try {
+        if(err) throw err
+        console.log(`listining port: ${PORT}`)
+
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-const server = app.listen(PORT, () => {
-    console.log(`Escuchando en el puerto: ${server.address().port}`)
-})
+// const server = app.listen(PORT, () => {
+//     console.log(`listining port: ${server.address().port}`)
+// })
 
-server.on('error', (err) => console.log(err))
+// server.on('error', (err) => console.log(err))
