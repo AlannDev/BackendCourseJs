a49489d702ab15500cdc04f64c568c7ddf34b280
Alan Diaz <aldiaz@arredo.com.ar>
2022-08-18 22:10:22 -0300

    Desafio motores de Plantillas

--- pug/server.js
+++ pug/server.js
@@ -1,42 +1,102 @@
-const express = require('express');
 
-const app = express();
-app.use( express.urlencoded({extended:true}))
-const port = 4000 || process.env.PORT
+const express = require('express')
+const app = express()
+const { Router} = express
+const router = Router()
+const Contenedor = require("./contenedor")
+const contenedor = new Contenedor("./productos.txt")
+app.use(express.json())
+app.use(express.static("public"))
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.use( express.urlencoded({extended: true}) )
 
-let productos = []
+const port = process.env.PORT || 4000
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        productos
-    })
+app.set('view engine', 'pug')
+app.set('views', './views')
+app.use("/api", router)
+const productos = []
+
+//configurar get 
+router.get('/productos', async(req, res) => {
+    try {
+        const productos1 = await contenedor.getAll()
+        productos.push(productos1)
+          res.render('pages/productos',{
+              mensaje: 'Hola ejs',
+              productos:productos1
+          })
+        } catch (error) {
+            res.json({
+                error
+            })
+        }
+
+} )
+router.get('/', async(req, res) => {
+    try {
+        const productos1 = await contenedor.getAll()
+      productos.push(productos1)
+        res.render('pages/index',{
+            mensaje: 'Hola ejs',
+            productos:productos1
+        })
+        
+    } catch (error) {
+        res.json({
+            error
+        })
+    }
+} )
+
+
+
+router.post('/productos', async (req, res) => {
+    try {
+        const obj = req.body
+        contenedor.save(obj)
+    
+        console.log(obj)
+        
+        productos.push(obj)
+        res.render('pages/index',{
+            mensaje: 'Hola ejs',
+            productos:productos
+        })
+        
+    } catch (error) {
+        res.json({
+            error
+        })
+    }
+
 })
 
-app.get('/cargarProducto', (req, res) => {
-    res.render('pages/create', {
-        mensaje: 'Hola ejs',
-        productos
-    })
-})
 
-app.post('/productos', (req, res) => {
-    const obj = req.body
-    productos.push(obj)
-    res.render('pages/create', {
-        // productos
-    })
-})
 
-app.get('/productos', (req, res) => {
-    res.render('pages/index', {
-        productos
-    })
-})
+app.set("view engine","pug")
+app.set("views","./views")
 
-app.listen(4000, err => {
-    if(err) throw new Error(`Error on server: ${err}`)
-    console.log(`Server is running on port ${port}`)
+// app.get("/",(req,res)=>{
+//     res.render("index.pug",{mensaje:"hola pugGGObjeto"})
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, err =>{
+if(err) throw new Error (`error on server listen${err}`)
+else{
+    console.log(`server on running on port ${port}`)
+}
 })
