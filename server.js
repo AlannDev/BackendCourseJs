37672c56b4d7b9579ac8b42c5349352962cff773
Alan Diaz <aldiaz@arredo.com.ar>
2022-09-01 23:11:54 -0300

    Desafio primera base de datos

--- server.js
+++ server.js
@@ -1,26 +1,82 @@
-const express = require('express')
+// server
+
+const express=require ('express')
+const { Server: HttpServer } = require('http')
+const { Server: Socket } = require('socket.io')
+const { Router } = express;
+
 const app = express()
-const dotenv = require('dotenv').config()
+const httpServer = new HttpServer(app)
+const io = new Socket(httpServer)
+
+app.use(express.static('public'))
+const dotenv = require ('dotenv').config()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const routerProductos = Router();
+
+const {options}=require('./Db/mariaDB/conexionDB')
+const {optionsSQ}=require('./Db/sqlite3/conexionDB')
+
+const KnexMDB= require('knex')(options)
+const KnexSqLite= require('knex')(optionsSQ)
+
+// archivo productos
+const Contenedor=require('./Api/Contenedor.js')
+const productos = new Contenedor(KnexMDB,'productos');
+
+// archivo comentarios
+const Comentarios= require('./Api/ComentariosArray')
+const mensajes = new Comentarios(KnexSqLite, 'mensajes');
+
+app.get('/',(req,res)=>{
+    res.render('/index.html',{root: __dirname})
+})
+
+
+
+io.on('connection', (socket)=>{
+    console.log('usuario conectado')
+    
+    // cargar 
+    const listaProductos = async () => {
+        let prods = await productos.getAll()
+        io.sockets.emit('lista-productos', prods)
+    }
+
+    const listaMensajes =  () => {
+        let msj =  mensajes.getAllComments
+        console.log(msj)
+        io.sockets.emit('lista-mensajes', msj)
+    }
+
+
+
+    socket.on('connection',async data=>{
+        await listaProductos()
+        await listaMensajes()
+
+    })
+
+    socket.on('producto-nuevo', async data => {
+        console.log('producto nuevo')
+        await productos.save(data)
+        await listaProductos()
+    })
+    
+    socket.on('mensaje-nuevo', async data => {
+        console.log('mensaje nuevo')
+        await mensajes.save(data)
+        await listaMensajes()
+    })
+    
+})
+
+app.use("/api/productos", routerProductos);
+
+// config puerto
 const PORT = process.env.PORT
 
-const routerProd = require('./router/routesProduct')
-const routerCart = require('./router/routesCart')
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-
-const pageNotFound = (req, res, next) => {
-    let error = new Error()
-    error.status = 404
-    res.json(`Error  ${error.status} - Página no existente`)
-    next()
-}
-
-app.use('/api/productos', routerProd)
-app.use('/api/carrito', routerCart)
-app.use(pageNotFound)
-
-app.listen(PORT, () => {
+httpServer.listen(PORT,()=>{
     console.log(`Escuchando en puerto ${PORT}`)
 })
